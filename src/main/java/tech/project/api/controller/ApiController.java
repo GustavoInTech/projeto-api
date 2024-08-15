274b6b03564fdbc3c7deb0700beb3b38a20c1331
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-14 21:39:32 -0300

    Finaliza ajustes e adiciona suporte básico para autenticação JWT

--- src/main/java/tech/project/api/controller/ApiController.java
+++ src/main/java/tech/project/api/controller/ApiController.java
@@ -4,16 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tech.project.api.model.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/tasks")
+@RequestMapping
 public class ApiController {
-
-    private List<String> tasks = new ArrayList<>();
-
+    private List<Task> tasks = new ArrayList<>();
     private ObjectMapper objectMapper;
 
     public ApiController(ObjectMapper objectMapper) {
@@ -21,13 +20,34 @@ public class ApiController {
     }
 
     @GetMapping(path = "/tasks")
-    public ResponseEntity<String>listTasks() throws JsonProcessingException {
+    public ResponseEntity<String> listTasks() throws JsonProcessingException {
         return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
     }
 
     @PostMapping(path = "/tasks")
-    public ResponseEntity<Void> createTask(@RequestBody String task){
+    public ResponseEntity<Void> createTask(@RequestBody Task task){
+        task.setId((long) (tasks.size() + 1)); // Simples auto-incremento de ID
         tasks.add(task);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(path = "/tasks/{id}")
+    public ResponseEntity<Void> updateTask(@PathVariable Long id, @RequestBody Task updateTask) {
+        for (Task task : tasks){
+            if (task.getId().equals(id)){
+                task.setTitle(updateTask.getTitle());
+                task.setDescription(updateTask.getDescription());
+                task.setPriority(updateTask.getPriority());
+                task.setStatus(updateTask.getStatus());
+                task.setDueDate(updateTask.getDueDate());
+                return ResponseEntity.ok().build();
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<Void> deletetask(@PathVariable Long id) {
+        tasks.removeIf(task -> task.getId().equals(id));
         return ResponseEntity.ok().build();
     }
 
